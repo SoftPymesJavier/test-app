8ce99482c775d42291440570ecdb79fb4ba05bb9
Aura Jimenez <auracristina.jimenezg@gmail.com>
2020-10-06 18:01:55 -0500

    Prueba

--- test_app/app/api/example/example_endpoint.py
+++ test_app/app/api/example/example_endpoint.py
@@ -3,9 +3,45 @@
 from flask import request, jsonify
 from app.api import api
 from app.controllers import ExampleController
-
+from app.models import Example
 
 @api.route('/example', methods=['GET'])
 def example():
     message = ExampleController.get_example()
     return jsonify(data=message)
+
+#nuevo example
+@api.route('/example', methods=['POST'])
+def create_example2():
+	new_example = ExampleController.create_example()
+
+	return jsonify(data=new_example)
+
+# Listar los example 
+@api.route('/examples', methods=['GET'])
+def example_listed2():
+	examples = ExampleController.example_listed()
+
+	return jsonify(data=examples)
+
+# Filtrar los examples por tiitulo
+@api.route('/example/<title>', methods=['GET'])
+def example_filtered2(title):
+	result = ExampleController.example_filtered(title)
+	
+	return jsonify(data=result)
+
+# actualizar example
+@api.route('/example/<id>', methods=['PUT'])
+def update_example2(id):
+	example = ExampleController.update_example(id)
+
+	return jsonify(data=example)
+
+
+# eliminar example
+@api.route('/example/<id>', methods=['DELETE'])
+def delete_example2(id):
+	example = ExampleController.delete_example(id)
+
+	return jsonify(data=example)
--- test_app/app/controllers/example/example_controller.py
+++ test_app/app/controllers/example/example_controller.py
@@ -3,8 +3,49 @@
 from app.models import Example
 
 
-class ExampleController:
+class ExampleController():
 
     @staticmethod
     def get_example():
         return 'Hello from Flask'
+
+    #nuevo example
+    @staticmethod
+    def create_example():
+    	title = Example.title
+    	description = Example.description
+    	new_example = Example(title, description)
+    	session.add(new_example)
+    	session.commit()
+    	return new_example
+
+    # Listar los example 
+    @staticmethod
+    def example_listed():
+    	examples = Example.objects.all()
+    	return examples
+
+    # Filtrar los examples por tiitul
+    @staticmethod
+    def example_filtered(title):
+    	result = Example.objects.filter(title=title)
+    	return result
+
+    # actualizar example
+    @staticmethod
+    def update_example(exampleId):
+    	example = Example.objects.get(exampleId=exampleId)
+    	title = request.json['title']
+    	description = request.json['description']
+    	example.title = title
+    	example.description = description
+    	session.commit()
+    	return example
+
+    # eliminar un ecxample
+    @staticmethod
+    def delete_example(exampleId):
+    	example = Example.objects.get(exampleId=exampleId)
+    	session.delete(example)
+    	session.commit()
+    	return example
